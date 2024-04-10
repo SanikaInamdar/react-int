d46069b74f70473661201ab78897981a75358dc3
Sanika Inamdar <86364275+SanikaInamdar@users.noreply.github.com>
2024-04-10 18:49:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React  , {useState} from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("")
+
+  const getTodo = () =>{
+    Axios.get("https://2848-61-95-158-116.ngrok-free.app/todos").then((response)=>{
+      console.log(response.data);
+      setTodos(response.data)
+    })
+  }
+  const postTodo = () => {
+    Axios.post("https://2848-61-95-158-116.ngrok-free.app/todos", {
+      title: title,
+      checked: false
+    }).then((response)=>{
+      console.log(response)
+    })
+  }
+  const deleteTodo = (id) => {
+    Axios.delete(`https://2848-61-95-158-116.ngrok-free.app/todos/${id}`).then((response)=>{
+      console.log(response)
+      // alert("todo delete!")
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>CRUD APIS</h1>
+        <button onClick={getTodo}> Get Todo </button>
+        {todos.map((todo, key)=>{
+          return (<div key={todo.id}>
+            <div>{todo.title}</div>
+            <div>{todo.checked === false ? "Not Done" : "Done"}</div>
+            <button onClick={()=>{deleteTodo(todo.id)}}> Delete a todo</button>
+            </div>
+          )
+        })}
+        <input name="title" placeholder='title' onChange={(e)=>{
+          setTitle(e.target.value)
+        }}/>
+    
+        <button onClick={postTodo}> Post Todo </button>
+      </div>
     </div>
   );
 }
